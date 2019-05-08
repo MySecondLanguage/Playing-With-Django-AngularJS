a75d0fbd4486d07d9848fda8c5521e31a06baf87
MySecondLanguage <mmamunism@gmail.com>
2019-05-08 23:24:35 +0600

    losing ui-view, got success in sending data to server with form

--- mysite/ng/models.py
+++ mysite/ng/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Contact(models.Model):
-    userid = models.IntegerField(unique=True)
+    userid = models.IntegerField()
     name = models.CharField(max_length=50)
     email = models.EmailField()
     phone = models.IntegerField()
--- mysite/ng/static/inputANDdisplay.js
+++ mysite/ng/static/inputANDdisplay.js
@@ -1,3 +1,5 @@
+/**
+
 app.controller('display', ['$scope', '$routeParams', function($scope, $routeParams) {
     $scope.userid = $routeParams.userid;
     $scope.name = $routeParams.name;
@@ -6,14 +8,33 @@ app.controller('display', ['$scope', '$routeParams', function($scope, $routePara
 }]);
 
 app.controller('inputCtrl', ['$scope', '$location', '$interpolate', function($scope, $location, $interpolate) {
-    $scope.userid = 0;
-    $scope.name = 'YOUR NAME';
-    $scope.email = 'jhonedoe@mail.com';
-    $scope.phone = "0058";
+    $scope.userid = null;
+    $scope.name = null;
+    $scope.email = null;
+    $scope.phone = null;
 
     $scope.PassDataToDisplyThroughUrl = function() {
         var url = $interpolate('/sumurl/{{ userid }}/{{ name }}/{{ email }}/{{ phone }}')($scope);
-        //console.log(url);
+        console.log(url);
         $location.path(url);
-    }
+    };
 }]);
+
+**/
+
+app.controller('inputCtrl', function ($scope, $http) {
+    $scope.formModel = {};
+
+    $scope.onSubmit = function () {
+        console.log("hey, i am submitting");
+        console.log($scope.formModel);
+
+        $http.post('http://127.0.0.1:8000/api/v1/contact/', $scope.formModel).
+        success(function (data) {
+            console.log(":)");
+        }).error(function (data) {
+            console.log(":(");
+        });
+    };
+});
+
